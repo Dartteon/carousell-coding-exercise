54d296386a08b0cc1f5151b0d3e3357d043d7bb4
Baron <a0122545@u.nus.edu>
2017-04-03 15:08:47 +0800

    Fix issue on Thread model where model was not exported

--- models/Thread.js
+++ models/Thread.js
@@ -3,9 +3,11 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 
 const threadSchema = new mongoose.Schema({
-    id: { type: String, unique: true },
     title: { type:String },
     description: { type: String},
     upVotes: Number,
     downVotes: Number
 }, { timestamps: true });
+
+const Thread = mongoose.model('Thread', threadSchema);
+module.exports = Thread;
